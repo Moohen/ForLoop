7bec4d325e29cf85fef678a10e6dad945007f336
Moohen <matt.moohen@gmail.com>
2017-12-06 13:41:22 +0100

    Final solution of problem: counting prime numbers

--- src/com/moohen/Main.java
+++ src/com/moohen/Main.java
@@ -3,6 +3,36 @@ package com.moohen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int count = 0;
+        for (int i = 10; i <50;i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if (count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
     }
+
+
+    private static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
 }
